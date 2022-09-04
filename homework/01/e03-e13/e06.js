edafc4b6d9ff92a5f5b9a4632da913c8af8e3cc1
Minna Nguyen <minna.nguyen.tuni.fi>
2022-09-04 20:16:35 +0300

    make commenting more readable

--- homework/01/e03-e13/e06.js
+++ homework/01/e03-e13/e06.js
@@ -4,23 +4,23 @@ function canYouSeeMe() {
     var f;
     var a = "var";
     let b = "let";
-    b = "LET let change";
+    b = "let 'let variable' change";
     const c = "const";
-    // c = "naah you can't change me!"; -> can't change the value because it's a const
+    /*c = "naah you can't change me!"; -> can't change the value because it's a const*/
     console.log("INSIDE if block:");
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log(d);
-    f = "hoist me?";
+    console.log(a); // output: var
+    console.log(b); // output: LET let me change
+    console.log(c); // output: const
+    console.log(d); // output: this var is..
+    f = "var: hoist me?";
     console.log(f);
     console.log(`${e}\n`); // all work because are declared in this same if block
   }
   console.log("OUTSIDE if block:");
   console.log(`${a}\n`);
-  //   console.log(b);
-  //   console.log(c);
-  // let and const don't work here because they're outside of the if block
+  /*  console.log(b);
+    console.log(c);
+  let and const don't work here because they're outside of the if block*/
   console.log(d);
   console.log(e);
   console.log(f);
