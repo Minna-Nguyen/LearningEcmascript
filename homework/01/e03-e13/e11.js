3ab15397b010a36ca91a3429bec5887f85ce6379
Minna Nguyen <minna.nguyen@tuni.fi>
2022-09-05 23:29:55 +0300

    e11.js: done

---
+++ homework/01/e03-e13/e11.js
@@ -0,0 +1,11 @@
+let cars = ["SAAB", "VOLVO", "BMW", "TOYOTA", "BENTLEY"];
+// using for loop
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+
+console.log();
+// using for of
+for (let x of cars) {
+  console.log(x);
+}
