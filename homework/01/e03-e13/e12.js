ed6d1fa58401751ee3f49efedbd433b93ba989e6
Minna Nguyen <minna.nguyen@tuni.fi>
2022-09-06 00:34:16 +0300

    e12.js: replace built-in method trim() with self made method, delete extra lines

--- homework/01/e03-e13/e12.js
+++ homework/01/e03-e13/e12.js
@@ -1,25 +1,30 @@
-let readLineSync = require("readLine-sync");
-let userInput = readLineSync.question("Please give any string: ");
-
-function isPalindrome(userStr) {
-  let forwards = "";
-  let backwards = "";
-  // check str forwards
+function trimWhiteSpaces(userStr) {
+  let trimmedStr = "";
   for (let i = 0; i < userStr.length; i++) {
-    forwards += userStr.charAt(i);
+    if (userStr.charAt(i) !== " ") {
+      trimmedStr += userStr.charAt(i);
+    }
   }
+  return trimmedStr;
+}
+
+function isPalindrome(checkUserStr) {
+  let backwards = "";
   // check str backwards
-  for (let i = userStr.length; i >= 0; i--) {
-    backwards += userStr.charAt(i);
+  for (let i = checkUserStr.length; i >= 0; i--) {
+    backwards += checkUserStr.charAt(i);
   }
   // compare these two strings
   // is not case sensitive
   // trim() removes spaces from both sides of the string
-  if (forwards.toUpperCase().trim() === backwards.toUpperCase().trim()) {
+  if (checkUserStr.toUpperCase() === backwards.toUpperCase()) {
     console.log("Congrats! You're given string is a palindrome!");
   } else {
     console.log("Sorry, you're given string is not a palindrome.");
   }
 }
 
-isPalindrome(userInput);
+let readLineSync = require("readLine-sync");
+let userInput = readLineSync.question("Please give any string: ");
+let strNoWhiteSpaces = trimWhiteSpaces(userInput); // trims the str first
+isPalindrome(strNoWhiteSpaces);
