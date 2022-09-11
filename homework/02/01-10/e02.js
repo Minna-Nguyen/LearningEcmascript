e8877c7cbce6e8e2b35ed5d3e21827e8076326e8
Minna Nguyen <minna.nguyen@tuni.fi>
2022-09-11 21:09:59 +0300

    e02.js: done

---
+++ homework/02/01-10/e02.js
@@ -0,0 +1,13 @@
+// constructor Person
+function Person(name, age) {
+  //this.x -> the name of the key
+  this.namee = name;
+  this.age = age;
+}
+
+let tiina = new Person("tiina", 234);
+console.log(tiina);
+
+// for (let x in tiina) {
+//   console.log(tiina[x]);
+// }
