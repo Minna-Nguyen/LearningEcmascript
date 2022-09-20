e3c558ad0f4ac0a86924a5d837db60760b02db11
Minna Nguyen <minna.nguyen@tuni.fi>
2022-09-20 23:24:20 +0300

    part 04 done

--- homework/03/03-04/index.js
+++ homework/03/03-04/index.js
@@ -1,24 +1,65 @@
 var fs = require("fs");
 var readline = require("readline-sync");
 
+// function doIt() {
+//   var userInput = readline.question("What is the name of the file? ");
+//   //ask name for the file -> create the file with that name
+//   // if no errors, read the file content
+//   fs.writeFile(`${userInput}`, "helloooo", (err, data) => {
+//     if (err) {
+//       return false;
+//     } else {
+//       fs.readFile(`${userInput}`, "utf-8", (err, data) => {
+//         if (err) {
+//           return false;
+//         } else {
+//           console.log(`filename: ${userInput}`);
+//           console.log(data);
+//         }
+//       });
+//     }
+//   });
+// }
+
+/* start of 04 part*/
+
 function doIt() {
   var userInput = readline.question("What is the name of the file? ");
-  //ask name for the file -> create the file with that name
-  // if no errors, read the file content
-  fs.writeFile(`${userInput}.txt`, "helloooo", (err, data) => {
-    if (err) {
-      return false;
-    } else {
-      fs.readFile(`${userInput}.txt`, "utf-8", (err, data) => {
-        if (err) {
-          return false;
-        } else {
-          console.log(`filename: ${userInput}.txt`);
-          console.log(data);
+  let checkIfExist = fileExist(userInput);
+  //if file exist, read the content
+  if (checkIfExist) {
+    fs.readFile(`${userInput}`, "utf-8", (err, data) => {
+      //err occurs if reading data fails
+      if (err) {
+        throw err;
+      } else {
+        //throw exception if file is not JSON
+        try {
+          const obj = JSON.parse(data);
+          //check if the json file has property "name"
+          if ("name" in obj) {
+            console.log(obj["name"]);
+          } else {
+            console.log(
+              `file ${userInput} contains json but not property name`
+            );
+          }
+
+          // console.log("name" in obj);
+        } catch (err) {
+          console.log(`file ${userInput} does not contain json`);
         }
-      });
-    }
-  });
+      }
+    });
+  }
 }
 
+function fileExist(data) {
+  let fileExist = fs.existsSync(`./${data}`);
+  if (!fileExist) {
+    console.log("file does not exist");
+  } else {
+    return true;
+  }
+}
 doIt();
